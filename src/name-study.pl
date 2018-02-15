d090f969dac99eb60d13f6e2bfa85494b04cb92d
Sam Page <sam.page@gmail.com>
2018-02-14 21:54:48 -0500

    initial name rules

--- src/name-study.pl
+++ src/name-study.pl
@@ -2,6 +2,21 @@
 
 # name study (TEMP)
 
+open(STOP,'<../dat/stopwords.txt') ||
+	die 'ERROR: stopwords dataset required';
+while(<STOP>){
+	chomp;
+	if( /^[a-z]/ ) { $lookupStop{$_}=1 }
+}
+close(STOP);
+
+open(HG,'<../out/homographs.txt') ||
+	die 'ERROR: homographs dataset required';
+while(<HG>){
+	chomp;
+	if( /^[a-z]/ ) { $lookupHg{$_}=1 }
+}
+close(HG);
 
 
 # read from the filter dataset
@@ -12,7 +27,18 @@ while(<DATASET>){
 	chomp;
 	($freq,$wordCount,$phones,$wordList) = split /\t/;
 	@words = split(/,/, $wordList);
-	print join("\t",@words) . "\n";
+
+	$nm = $words[0];
+	foreach my $w (@words){
+		next if ($lookupStop{$w});
+		next if ($lookupHg{$w});
+		next if (length($w)<3);
+		next if ($lookupGroups{$w});
+		$nm = $w;
+		$lookupGroups{$w}=1;
+		last;
+	}
+	print join("\t",$nm,@words) . "\n";
 }
 close(DATASET);
 
