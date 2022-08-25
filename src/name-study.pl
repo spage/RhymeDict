94cedafc1cba58f26e69b1f84fd485d647102c6e
Sam Page <spage@liquidhub.com>
2022-08-25 18:17:35 -0400

    review and comments on name

--- src/name-study.pl
+++ src/name-study.pl
@@ -2,6 +2,8 @@
 use strict;
 
 # name study (TEMP)
+# pick the word that best represents the list of words
+# words should be in usage frequency order to penalize obscure words
 
 open(STOP,'<../dat/stopwords.txt') ||
 	die 'ERROR: stopwords dataset required';
@@ -32,14 +34,22 @@ while(<DATASET>){
 	(my $freq, my $wordCount, my $phones, my $wordList) = split /\t/;
 	my @words = split(/,/, $wordList);
 
+	# choose the first word as default
+	# falls through if a better word is not found
 	my $nm = $words[0];
 	foreach my $w (@words){
+		# don't choose a stop word
 		next if ($lookupStop{$w});
+		# don't choose a homograph (two prons same sp)
 		next if ($lookupHg{$w});
+		# don't choose a too short word
 		next if (length($w)<3);
+		# don't choose a word already chosen
 		next if ($lookupGroups{$w});
+		# take the first word meeting these criteria
 		$nm = $w;
 		$lookupGroups{$w}=1;
+		#stop looking
 		last;
 	}
 	print join("\t", $nm, sort @words) . "\n";
