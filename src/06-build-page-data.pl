5fb330c30d6c013d05e579f3a7a736d6c55a4bf6
Sam Page <spage@liquidhub.com>
2022-09-06 17:03:28 -0400

    group over progress, edits

--- src/06-build-page-data.pl
+++ src/06-build-page-data.pl
@@ -8,12 +8,13 @@ open(DATASET,'<../out/group-dataset.txt') ||
 	die 'ERROR: group dataset required';	
 while(<DATASET>){
 	chomp;
+	next unless( /^[A-Z]/ );
 
 	(my $group, my $name, my $wc, my $words, my $ends) = split /\t/;
 
 	# only build for pages with more than 5 words
 	next if ($wc =~ /^[12345]$/);
 	
-	print join("\t", $name, $words) . "\n";
+	print join("\t", $name, $words, $group, $name) . "\n";
 }
 close(DATASET);
