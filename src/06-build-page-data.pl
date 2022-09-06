1537b591b0c9dff8d68408ac0e923bdb3a328fef
Sam Page <spage@liquidhub.com>
2022-09-06 14:52:20 -0400

    begin page data build

---
+++ src/06-build-page-data.pl
@@ -0,0 +1,19 @@
+#!perl 06-build-page-data.pl
+use strict;
+
+# build page data files
+
+# load group dataset
+open(DATASET,'<../out/group-dataset.txt') ||
+	die 'ERROR: group dataset required';	
+while(<DATASET>){
+	chomp;
+
+	(my $group, my $name, my $wc, my $words, my $ends) = split /\t/;
+
+	# only build for pages with more than 5 words
+	next if ($wc =~ /^[12345]$/);
+	
+	print join("\t", $name, $words) . "\n";
+}
+close(DATASET);
