0cf555d30c111d780b5259ddc68c1025b25b947e
Sam Page <sam.page@gmail.com>
2023-11-20 17:06:42 -0500

    ongoing edits frog,dog

--- src/06-build-page-data.pl
+++ src/06-build-page-data.pl
@@ -13,13 +13,13 @@ while(<DATASET>){
 	(my $group, my $name, my $wc, my $words, my $ends) = split /\t/;
 
 	# only build for pages with more than 5 words
-	next if ($wc =~ /^[12345]$/);
+	#next if ($wc =~ /^[12345]$/);
 	
 	# only build for pages with 2-5 words
 	#next unless ($wc =~ /^[2345]$/);
 
 	# only build for pages with 1 word
-	#next unless ($wc =~ /^1$/);
+	next unless ($wc eq '5');
 
 	print join("\t", $name . "(" . $wc . ")", $words) . "\n";
 }
